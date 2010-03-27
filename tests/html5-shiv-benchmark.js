64f2b42376a8136a53bfe3c3c03eea85611f8b3e
Lindsay Evans <linz@linz.id.au>
2010-03-27 13:14:21 +1100

    Adding unrolled loop to benchmarks

--- tests/html5-shiv-benchmark.js
+++ tests/html5-shiv-benchmark.js
@@ -1,45 +1,44 @@
 var methods = {
 
+unrolled: function(){document.createElement('abbr');document.createElement('article');document.createElement('aside');document.createElement('audio');document.createElement('canvas');document.createElement('details');document.createElement('figcaption');document.createElement('figure');document.createElement('footer');document.createElement('header');document.createElement('hgroup');document.createElement('mark');document.createElement('menu');document.createElement('meter');document.createElement('nav');document.createElement('output');document.createElement('progress');document.createElement('section');document.createElement('summary');document.createElement('time');document.createElement('video')},
+
 // Original: http://html5shiv.googlecode.com/svn/trunk/html5.js
 original: function(){var e = "abbr,article,aside,audio,canvas,details,figcaption,figure,footer,header,hgroup,mark,menu,meter,nav,output,progress,section,summary,time,video".split(','),i=e.length;while(i--){document.createElement(e[i])}},
 
 // Updated original
 original2: function(){'abbr article aside audio canvas details figcaption figure footer header hgroup mark menu meter nav output progress section summary time video'.replace(/\w+/g,function(n){document.createElement(n)})},
 
-// 1: String.split to native array
 array_literal: function(){var e=['abbr','article','aside','audio','canvas','details','figcaption','figure','footer','header','hgroup','mark','menu','meter','nav','output','progress','section','summary','time','video'],i=e.length;while(i--){document.createElement(e[i])}},
 
 array_constructor: function(){var e=new Array('abbr','article','aside','audio','canvas','details','figcaption','figure','footer','header','hgroup','mark','menu','meter','nav','output','progress','section','summary','time','video'),i=e.length;while(i--){document.createElement(e[i])}},
 
-// 2: caching document object
 caching_document: function(){var e=['abbr','article','aside','audio','canvas','details','figcaption','figure','footer','header','hgroup','mark','menu','meter','nav','output','progress','section','summary','time','video'],i=e.length,d=document;while(i--){d.createElement(e[i])}},
 
-// 3: static array length
-static_array_length: function(){var e=['abbr','article','aside','audio','canvas','details','figcaption','figure','footer','header','hgroup','mark','menu','meter','nav','output','progress','section','summary','time','video'],i=21,d=document;while(i--)d.createElement(e[i]);},
+static_array_length: function(){var e=['abbr','article','aside','audio','canvas','details','figcaption','figure','footer','header','hgroup','mark','menu','meter','nav','output','progress','section','summary','time','video'],i=21,d=document;while(i--)d.createElement(e[i])},
 
-array_constructor_static_array_length: function(){var e=new Array('abbr','article','aside','audio','canvas','details','figcaption','figure','footer','header','hgroup','mark','menu','meter','nav','output','progress','section','summary','time','video'),i=21,d=document;while(i--)d.createElement(e[i]);},
+array_constructor_static_array_length: function(){var e=new Array('abbr','article','aside','audio','canvas','details','figcaption','figure','footer','header','hgroup','mark','menu','meter','nav','output','progress','section','summary','time','video'),i=21,d=document;while(i--)d.createElement(e[i])},
 
-array_constructor_static_array_length_nocache: function(){var e=new Array('abbr','article','aside','audio','canvas','details','figcaption','figure','footer','header','hgroup','mark','menu','meter','nav','output','progress','section','summary','time','video'),i=21;while(i--)document.createElement(e[i]);}
+array_constructor_static_array_length_nocache: function(){var e=new Array('abbr','article','aside','audio','canvas','details','figcaption','figure','footer','header','hgroup','mark','menu','meter','nav','output','progress','section','summary','time','video'),i=21;while(i--)document.createElement(e[i])}
 
 };
 
-/*
-
+// *
 document.write('<h2>Byte size:</h2><dl>');
 for(var name in methods){
-    document.write('<dt>'+name+'</dt><dd>'+methods[name].toString().replace(' ', '').length+' bytes</dd>');
+    document.write('<dt>'+name+'</dt><dd>'+methods[name].toString().replace(' ', '').replace('var','var ').replace('new','new ').length+' bytes</dd>');
 }
 document.write('</dl>');
-/ */
+// */
 
+// *
 document.write('<h2>Benchmarks:</h2>');
 
 var bm = new Benchmark(
   methods,
   {
     responders: 'Perlish',
-    iterations:  -2,
-    cooldown:    1000
+    iterations:  -10,
+    cooldown:    30000
   }
 
 );

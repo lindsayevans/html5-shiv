e9c955b6a3aaea76e40ce3741b2a8e8103383685
Lindsay Evans <linz@linz.id.au>
2010-10-06 07:47:04 +1100

    Shifting vars into args

--- html5-shiv.js
+++ html5-shiv.js
@@ -1,1 +1,1 @@
-/*@cc_on (function(){var e=['abbr','article','aside','audio','canvas','details','figcaption','figure','footer','header','hgroup','mark','menu','meter','nav','output','progress','section','summary','time','video'],i=21,d=document;while(i--)d.createElement(e[i])})()@*/
+/*@cc_on (function(e,i,d){while(i--)d.createElement(e[i])})(['abbr','article','aside','audio','canvas','details','figcaption','figure','footer','header','hgroup','mark','menu','meter','nav','output','progress','section','summary','time','video'],21,document)@*/

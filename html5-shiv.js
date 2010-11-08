3f4dbcd1928cbda3ee97f72f06686185ccfcc264
Lindsay Evans <linz@linz.id.au>
2010-11-09 00:05:04 +1100

    Refactoring so the args are d,i,e

--- html5-shiv.js
+++ html5-shiv.js
@@ -1,1 +1,1 @@
-/*@cc_on (function(e,i,d){while(i--)d.createElement(e[i])})(['abbr','article','aside','audio','canvas','details','figcaption','figure','footer','header','hgroup','mark','menu','meter','nav','output','progress','section','summary','time','video'],21,document)@*/
+/*@cc_on (function(d,i,e){while(i--)d.createElement(e[i])})(document,21,['abbr','article','aside','audio','canvas','details','figcaption','figure','footer','header','hgroup','mark','menu','meter','nav','output','progress','section','summary','time','video'])@*/
